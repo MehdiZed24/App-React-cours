10378b2341840e2c827430738695d94b4ca8761a
MehdiZed24 <mehdiziani1989@gmail.com>
2021-09-20 15:25:40 +0200

    Création de la première fonction avec affichage sur téléphone

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(63999);
+  function onAdd() {
+    setCount(count + 1);
+  }
+  
+  function onSub() {
+    setCount(count - 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titre}>Bienvenue!</Text>
+      <Text style={styles.titre}>{count}</Text>
+     <Button />
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
