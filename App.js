cf78c5f2fab85635dc2227a0d6687907f7edc148
MehdiZed24 <mehdiziani1989@gmail.com>
2021-09-20 16:21:55 +0200

    Création du premier composant:bouton

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Button from "./components/Button.js";
+import { useState } from "react";
+
+
 
 export default function App() {
-  const [count, setCount] = useState(63999);
+  const [count, setCount] = useState(0);
   function onAdd() {
     setCount(count + 1);
   }
-  
+
   function onSub() {
     setCount(count - 1);
   }
@@ -16,12 +20,22 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.titre}>Bienvenue!</Text>
       <Text style={styles.titre}>{count}</Text>
-     <Button />
-
-
+      <Button name="Ajouter" onClick={onAdd}/>
+      <Button name="Soustraire" onClick={onSub} />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+  },
+  titre: {
+    fontWeight: "bold",
+    padding: 25,
+  },
+});
---
+++ components/Button.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function Button(props) {
+
+
+  
+  return (
+    <TouchableOpacity onPress={props.onClick}>
+      <Text style={styles.bouton}>{props.name}</Text>
+    </TouchableOpacity>,
+    (
+      <TouchableOpacity onPress={props.onClick}>
+        <Text style={styles.bouton}>{props.name}</Text>
+      </TouchableOpacity>
+    )
+  );
+}
+
+const styles = StyleSheet.create({
+  bouton: {
+    backgroundColor: "#093357",
+    padding: 8,
+    margin: 5,
+    width: "50%",
+    color: "#bbb",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
