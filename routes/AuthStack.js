ff685906d28b80f5a6cfceee479dfd9b8f0e817f
MehdiZed24 <mehdiziani1989@gmail.com>
2021-09-21 17:06:57 +0200

    Fin de la journée 2

---
+++ routes/AuthStack.js
@@ -0,0 +1,9 @@
+/**
+ * Installation de react-navigation avec : 
+yarn add react-navigation
+
+puis
+yarn add react-navigation-stack
+et
+expo install react-native-gesture-handler react-native-reanimated
+ */
---
+++ screens/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from "react-native";
+import { globalStyles } from '../styles/GlobalStyles';
+
+
+export default function Signin() {
+  return (
+<View>
+  <Text style={globalStyles.titre}>SIGNIN</Text>
+</View>
+  )
+}
