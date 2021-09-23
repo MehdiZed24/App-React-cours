cd2dda048f1c2261a30cab2c1edc5acbdeab92d9
MehdiZed24 <mehdiziani1989@gmail.com>
2021-09-23 20:14:28 +0200

    Fin de la journée 4

--- components/Cam.js
+++ components/Cam.js
@@ -1,9 +1,45 @@
-import React, { useState } from 'react'
-  
+import React, { useState, useEffect } from "react";
+import { Camera } from "expo-camera";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function Cam() {
-    const [isPermission, setPermission] = useState(false)
+  const [isPermission, setIsPermission] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+
+  //useEffect s'execute quend le composant est monté
+
+  useEffect(() => {
+    //On utilise une fonction async qui renvoie "granted ou denied"
+    //Puis on met à jour notre paramètre isPermission suivant le statut.
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setIsPermission(status === "granted");
+    })();
+    //La fonction return s'exécute quand le composant est sur le point d'être démonté
+    //   return ()=>{
+    //   }
+  }, []);
+
+  if (isPermission === false) {
     return (
-      <div></div>
-      )
+      <View>
+        <Text>No access to camera</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={styles.camera} type={type}></Camera>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { globalStyles } from "../styles/GlobalStyles";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Modal } from "react-native";
 import { useContext } from "react";
 import Button from "../components/Button";
 import { UserContext } from "../contexts/UserContext";
+import Cam from "../components/Cam"
 
 UserContext;
 export default function Home() {
+  const [isCamera, setIsCamera] = useState(false)
   const context = useContext(UserContext);
 
   const handleLogout = () => {
@@ -15,7 +17,12 @@ export default function Home() {
   return (
     // mon composant home a une vue avec son style, son button qui a un nom (logout) et une fonction qui se déclenche au clicK.
     <View>
-      <Text style={globalStyles.titre}>Hosme</Text>
+      {/* Modal est un composant qui s'affiche selon une condition donnée en propriété nommée visible. */}
+      <Modal visible={isCamera}>
+        <Cam></Cam>
+      </Modal>
+      <Text style={globalStyles.titre}>Home</Text>
+      <Button name="Ouvrir Camera" onClick={()=>setIsCamera(true)}></Button>
       <Button name="Logout" onClick={handleLogout}></Button>
     </View>
   );
